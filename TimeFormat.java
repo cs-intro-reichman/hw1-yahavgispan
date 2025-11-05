2faa571e85b41dc51eb40bcde2941f0f561a291c
Yahav Gispan <yahav.gispan@post.runi.ac.il>
2025-11-06 00:42:53 +0200

    time format fix

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,17 +5,18 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         String time = "AM"; 
-		if (hours > 12 ){
+	    if (hours >= 12 ){
+			if (hours > 12){
 			hours = hours - 12;
+			}
 			time = "PM";
-		} 
-		if (minutes < 10){
-			System.out.println( hours + ":0" + minutes + " " + time);
-
-		}
-		else{
-		   System.out.println( hours + ":" + minutes + " " + time);
-
-		}
 	}
+	    if (minutes < 10){
+			System.out.println( hours + ":0" + minutes + " " + time);
+		}
+	    else{
+		   System.out.println( hours + ":" + minutes + " " + time);
+		}
+	
+}
 }
